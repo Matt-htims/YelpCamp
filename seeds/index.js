a5b4fc16a161e338cedcdee96c9173dbbef5425f
Matt <matt.smith3108@gmail.com>
2021-01-14 13:57:00 +0000

    Updated seeds.js to properly seed the Mongo Atlas db

--- seeds/index.js
+++ seeds/index.js
@@ -4,11 +4,14 @@ const axios = require('axios');
 const { places, descriptors } = require('./seedHelpers');
 const Campground = require('../models/campground');
 
-mongoose.connect('mongodb://localhost:27017/yelp-camp', {
-	useNewUrlParser: true,
-	useCreateIndex: true,
-	useUnifiedTopology: true,
-});
+mongoose.connect(
+	'mongodb+srv://first-user:SBLiyERmC7g4jNE@cluster0.dzqvb.mongodb.net/<dbname>?retryWrites=true&w=majority',
+	{
+		useNewUrlParser: true,
+		useCreateIndex: true,
+		useUnifiedTopology: true,
+	}
+);
 
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
@@ -20,11 +23,11 @@ const sample = array => array[Math.floor(Math.random() * array.length)];
 
 const seedDB = async () => {
 	await Campground.deleteMany({});
-	for (let i = 0; i < 300; i++) {
+	for (let i = 0; i < 50; i++) {
 		const random1000 = Math.floor(Math.random() * 1000);
 		const price = Math.floor(Math.random() * 20) + 10;
 		const camp = new Campground({
-			author: '5ff5d8509506d0029a80d366',
+			author: '5fff2642f1ccc201494f64b1',
 			location: `${cities[random1000].city}, ${cities[random1000].state}`,
 			title: `${sample(descriptors)} ${sample(places)}`,
 			images: [
